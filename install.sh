9fc240371ed365b7691d6b058ea24d44ce9b9cdc
Shawn Deleurme <shawn@deleur.me>
2016-02-25 19:32:21 -0800

    Only works on linux, change the install script

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # vim:ft=sh
-
-OS=`uname`
-
-if [ $OS == "Darwin" -a -x `which brew` ]; then
-  cp lxc-completion /usr/local/etc/bash_completion.d
-else 
-  cp lxc-completion /etc/bash_completion.d/
-fi
+cp lxc-completion /etc/bash_completion.d/
